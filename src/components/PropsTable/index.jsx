a374d8965e90f7dd7789dc7bf10a3da5829d1e72
goprett <im.gopret@gmail.com>
2023-10-28 07:15:58 +0700

    tes solve eror

--- src/components/PropsTable/index.jsx
+++ src/components/PropsTable/index.jsx
@@ -1,143 +1,143 @@
-import React from 'react';
-import UIKitDocs from '@dytesdk/ui-kit/dist/docs/docs-components.json';
-import CodeBlock from '@theme/CodeBlock';
-import styles from './styles.module.css';
-import Link from '@docusaurus/Link';
+// import React from 'react';
+// import UIKitDocs from '@dytesdk/ui-kit/dist/docs/docs-components.json';
+// import CodeBlock from '@theme/CodeBlock';
+// import styles from './styles.module.css';
+// import Link from '@docusaurus/Link';
 
-const REFERENCE_PROPS = {
-  config: {
-    path: '../reference/interfaces/UIConfig',
-    name: 'UIConfig',
-  },
-  iconPack: {
-    path: '../reference/#iconpack',
-    name: 'IconPack',
-  },
-  t: {
-    path: '../reference/#dytei18n',
-    name: 'DyteI18n',
-  },
-  // participant: {
-  //   path: '../reference/participant',
-  //   name: 'Participant',
-  // },
-  states: {
-    path: '../reference/interfaces/States',
-    name: 'States',
-  },
-  notification: {
-    path: '../reference/interfaces/Notification',
-    name: 'Notification',
-  },
-  // poll: {
-  //   path: '../reference/poll',
-  //   name: 'Poll',
-  // },
-  plugins: {
-    path: '/java',
-    name: 'DytePlugin[]',
-  },
-  plugin: {
-    path: '/java',
-    name: 'DytePlugin',
-  },
-};
+// const REFERENCE_PROPS = {
+//   config: {
+//     path: '../reference/interfaces/UIConfig',
+//     name: 'UIConfig',
+//   },
+//   iconPack: {
+//     path: '../reference/#iconpack',
+//     name: 'IconPack',
+//   },
+//   t: {
+//     path: '../reference/#dytei18n',
+//     name: 'DyteI18n',
+//   },
+//   // participant: {
+//   //   path: '../reference/participant',
+//   //   name: 'Participant',
+//   // },
+//   states: {
+//     path: '../reference/interfaces/States',
+//     name: 'States',
+//   },
+//   notification: {
+//     path: '../reference/interfaces/Notification',
+//     name: 'Notification',
+//   },
+//   // poll: {
+//   //   path: '../reference/poll',
+//   //   name: 'Poll',
+//   // },
+//   plugins: {
+//     path: '/java',
+//     name: 'DytePlugin[]',
+//   },
+//   plugin: {
+//     path: '/java',
+//     name: 'DytePlugin',
+//   },
+// };
 
-const WEB_CORE_TYPE_REFERENCES = {
-  DyteClient: {
-    path: '/java',
-    name: 'DyteClient',
-  },
-};
+// const WEB_CORE_TYPE_REFERENCES = {
+//   DyteClient: {
+//     path: '/java',
+//     name: 'DyteClient',
+//   },
+// };
 
-function PropType({ prop }) {
-  if (prop.name === 'participant') {
-    return (
-      <pre className={styles.propType}>
-        <code>
-          <Link href="/java">
-            DyteParticipant
-          </Link>{' '}
-          | <Link href="/java">DyteSelf</Link>
-        </code>
-      </pre>
-    );
-  }
+// function PropType({ prop }) {
+//   if (prop.name === 'participant') {
+//     return (
+//       <pre className={styles.propType}>
+//         <code>
+//           <Link href="/java">
+//             DyteParticipant
+//           </Link>{' '}
+//           | <Link href="/java">DyteSelf</Link>
+//         </code>
+//       </pre>
+//     );
+//   }
 
-  if (prop.type === 'Peer[]') {
-    return (
-      <pre className={styles.propType}>
-        <code>
-          (
-          <Link href="/java">
-            DyteParticipant
-          </Link>{' '}
-          | <Link href="/java">DyteSelf</Link>)[]
-        </code>
-      </pre>
-    );
-  }
+//   if (prop.type === 'Peer[]') {
+//     return (
+//       <pre className={styles.propType}>
+//         <code>
+//           (
+//           <Link href="/java">
+//             DyteParticipant
+//           </Link>{' '}
+//           | <Link href="/java">DyteSelf</Link>)[]
+//         </code>
+//       </pre>
+//     );
+//   }
 
-  const ref = REFERENCE_PROPS[prop.name] || WEB_CORE_TYPE_REFERENCES[prop.type];
+//   const ref = REFERENCE_PROPS[prop.name] || WEB_CORE_TYPE_REFERENCES[prop.type];
 
-  if (ref) {
-    return (
-      <pre className={styles.propType}>
-        <code>
-          <Link href={ref.path}>{ref.name}</Link>
-        </code>
-      </pre>
-    );
-  }
+//   if (ref) {
+//     return (
+//       <pre className={styles.propType}>
+//         <code>
+//           <Link href={ref.path}>{ref.name}</Link>
+//         </code>
+//       </pre>
+//     );
+//   }
 
-  return <CodeBlock language="ts">{prop.type}</CodeBlock>;
-}
+//   return <CodeBlock language="ts">{prop.type}</CodeBlock>;
+// }
 
-function PropCard({ prop }) {
-  return (
-    <div className={styles.card}>
-      <h4>
-        <code>{prop.name}</code>
-      </h4>
-      <div>
-        {prop.required && (
-          <p>
-            <code data-code="required">required</code>
-          </p>
-        )}
-        <p className={styles.docs}>{prop.docs}</p>
-        {prop.default && prop.default.length > 0 && (
-          <div>
-            <h5>Default</h5>
-            <CodeBlock language="ts">{prop.default}</CodeBlock>
-          </div>
-        )}
-        <div>
-          <h5>Type</h5>
-          <PropType prop={prop} />
-        </div>
-      </div>
-    </div>
-  );
-}
+// function PropCard({ prop }) {
+//   return (
+//     <div className={styles.card}>
+//       <h4>
+//         <code>{prop.name}</code>
+//       </h4>
+//       <div>
+//         {prop.required && (
+//           <p>
+//             <code data-code="required">required</code>
+//           </p>
+//         )}
+//         <p className={styles.docs}>{prop.docs}</p>
+//         {prop.default && prop.default.length > 0 && (
+//           <div>
+//             <h5>Default</h5>
+//             <CodeBlock language="ts">{prop.default}</CodeBlock>
+//           </div>
+//         )}
+//         <div>
+//           <h5>Type</h5>
+//           <PropType prop={prop} />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default function PropsTable({ of }) {
-  const doc = UIKitDocs.components.find((component) => component.tag === of);
+// export default function PropsTable({ of }) {
+//   const doc = UIKitDocs.components.find((component) => component.tag === of);
 
-  if (!doc) return null;
+//   if (!doc) return null;
 
-  const { props } = doc;
+//   const { props } = doc;
 
-  // brings required props to start
-  props.sort((a) => (a.required ? -1 : 1));
+//   // brings required props to start
+//   props.sort((a) => (a.required ? -1 : 1));
 
-  return (
-    <div className={styles.root}>
-      <div className={styles.props}>
-        {props.map((prop) => (
-          <PropCard prop={prop} key={prop.name} />
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.root}>
+//       <div className={styles.props}>
+//         {props.map((prop) => (
+//           <PropCard prop={prop} key={prop.name} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
