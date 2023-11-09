1ae51bc8d827ca430ab9f069ba3aec5754c113b4
goprett <im.gopret@gmail.com>
2023-11-09 07:24:10 +0700

    .

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -10,6 +10,7 @@ export default function DocItemWrapper(props) {
   return (
     <>
       <DocItem {...props} />
+      <br/>
         <DiscussionEmbed
           shortname='mynotes-5'
           config={{
